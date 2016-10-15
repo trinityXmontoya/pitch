5bd1e7018756e16b2dc15612f8d164a7c40c0cf3
trinityXmontoya <trinityXmontoya@gmail.com>
2016-10-15 11:23:29 -0400

    merchandise locations

--- server/items.js
+++ server/items.js
@@ -20,5 +20,23 @@ Items = {
     _.each(results, function(result){
       StadiumItems.insert(result)
     })
+  },
+  locations: function(){
+    var url = "http://newyork.yankees.mlb.com/gdcross/components/game/mlb/mobile/ballpark/iphone/config/nyy_map.json"
+    var levels = HTTP.get(url).data["Yankee Stadium"]["Levels"]
+    var results =
+    _.flatten(
+      _.map(levels, function(level){
+        return _.map(level["Places"], function(place){
+          return {id: place.ID,
+                  location: place.Location,
+                   name: place.Name,
+                   section: place.Section,
+                   subType: place.SubType,
+                   type: place.Type,
+                   keywords: place.keywords}
+        })
+      })
+    )
   }
 }
--- server/main.js
+++ server/main.js
@@ -6,4 +6,5 @@ Meteor.startup(() => {
     console.log("seeding")
       Items.seed()
   }
+  Items.locations()
 });
