1d15c4bef86f603680c182b525aed946a9ba9f8d
trinityXmontoya <trinityXmontoya@gmail.com>
2016-10-15 11:03:52 -0400

    update item scrape + seed

--- client/items.js
+++ client/items.js
@@ -2,7 +2,6 @@ StadiumItems = new Mongo.Collection("Pitch.Items");
 
 Template.Items.helpers({
   items: function(){
-    console.log("findme", StadiumItems.find({}))
     return StadiumItems.find({})
   }
 })
